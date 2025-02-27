dcdc5ac19a034c3944081417454f279a8f7948ab
Popov-Roman <147161071+Popov-Roman@users.noreply.github.com>
2025-02-27 19:24:04 +0300

    Update Utils.cpp add GetKeyState

--- Utils.cpp
+++ Utils.cpp
@@ -1,8 +1,32 @@
 #include "Game.cpp"
 #include "Field.cpp"
+#include <Windows.h>
 
 void print_field(Field& f);
 void update_field(Game& g);
 
-bool strelka_vpravo(){};//getkeystate
-bool strelka_vlevo(){};//getkeystate
+bool strelka_vpravo() {
+    SHORT state = GetKeyState(68);
+    if (state < 0) {
+        return false;
+    }
+    else if (state > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+bool strelka_vlevo(){
+    SHORT state = GetKeyState(65);
+    if (state < 0) {
+        return false;
+    }
+    else if (state > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
